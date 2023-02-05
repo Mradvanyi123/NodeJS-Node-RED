f81dedbe90920eeaed1bb40cc9984a9253a69533
Mátyás Radványi <mradvanyi0122@gmail.com>
2023-02-05 22:37:49 +0100

    Node JS server initialization.

---
+++ nodeJS-server/main.js
@@ -0,0 +1,12 @@
+console.log("")
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use('/', require('./routes/endpoints'));
+
+// constants
+const port = 8081;
+
+app.listen(port, () => {
+    console.log(`API server listening on port ${port}`)
+})
